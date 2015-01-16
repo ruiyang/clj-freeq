b6ec49cdce32010879d0ddd9e11be43cc15a485d
ryang <ryang.sei@gmail.com>
2015-01-16 23:57:25 +1100

    spike insta parser

--- src/matchit/core.clj
+++ src/matchit/core.clj
@@ -1,6 +1,21 @@
-(ns matchit.core)
+(ns matchit.core
+  (:require [instaparse.core :as insta]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def exp "S = VALUE_EXP OPERATOR VALUE_EXP;
+          OPERATOR =#\"(<|>)\";
+          ATTR_ACCESSOR = #\"\\.\";
+          CHAR=#\"[A-Za-z]\";
+          NAME=CHAR+;
+          ATTR_ACCESS = #\"\\.\",NAME;
+          VALUE_EXP=NAME,ATTR_ACCESS")
+
+(def filter-func
+  (insta/parser exp))
+
+(filter-func "abc.t>def.g")
+
+;; (->> (filter-func "abc.t>def.g")
+;;   (insta/transform
+;;    {:> >, :< <
+;;     :number clojure.edn/read-string :expr identity}))
+
