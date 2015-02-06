662e0d6e3890045bfd150e5f786f9041b44821d7
Rui Yang <ryang.sei@gmail.com>
2015-02-07 10:54:15 +1100

    add support for function calls

--- src/matchit/core.clj
+++ src/matchit/core.clj
@@ -1,29 +1,39 @@
 (ns matchit.core
   (:require [instaparse.core :as insta]))
 
-(def data {:abc {:def true} :def {:gh false} :hh {:ii true} :gg {:ff true}})
-
 (def exp "EXP = (AND_EXPRESSION | GROUP_EXP) (<LOGICAL_OR> EXP)*
   <GROUP_EXP> = <SEPERATOR*> <\"(\"> <SEPERATOR*> EXP <SEPERATOR*> <\")\"> <SEPERATOR*>
   LOGICAL_AND = <SEPERATOR*> \"AND\" <SEPERATOR*>;
   LOGICAL_OR = <SEPERATOR*> \"OR\" <SEPERATOR*>;
-  AND_EXPRESSION = VALUE_EXP (<LOGICAL_AND> (VALUE_EXP | GROUP_EXP))*
+  AND_EXPRESSION = (VALUE_EXP | FUNC_CALL) (<LOGICAL_AND> (VALUE_EXP | GROUP_EXP | FUNC_CALL))*
+  FUNC_CALL = NAME <\"(\"> ((VALUE_EXP <FUNC_PARAMETER_SEPARATOR>)* VALUE_EXP |Epsilon) <\")\">
+  <FUNC_PARAMETER_SEPARATOR> = \",\"
   <SEPERATOR> = \" \"+
   <OPERATOR> =#\"(<|>)\";
-  NAME=#\"[A-Za-z]\"+;
+  NAME=#\"[A-Za-z]+\";
   VALUE_EXP=<SEPERATOR*> NAME(<#\"\\.\">NAME)* <SEPERATOR*>")
+
+(def customize-function-map {
+                             :equal =
+                             })
 
 (def transform-map {:ATTR_ACCESS (fn [& args] (keyword (nth args 1)))
                     :NAME (fn [& args] (apply str args))
                     :VALUE_EXP (fn [& args] (fn [data] (get-in data (map keyword args))))
                     :AND_EXPRESSION (fn [& args] (fn [data] (reduce #(and %1 (%2 data)) true args)))
-                    :OPERATOR (fn [& args] (fn [x y] (> x y)))
+                    :FUNC_CALL (fn [& args] (fn [data] (apply ((keyword (nth args 0)) customize-function-map) (map #(% data) (rest args)))))
                     :EXP (fn [& args] (fn [data] (reduce #(or %1 (%2 data)) false args)))
                     })
 
+(def data {:abc {:def true} :def {:gh false} :hh {:ii true} :gg {:ff true}})
+
 ((instaparse.core/transform
   transform-map
-  ((insta/parser exp) "abc.def AND (def.gh AND def.gg OR hh.ii)")) data)
+  ((insta/parser exp) "equal(abc.def, hh.ii)")) data)
+
+((instaparse.core/transform
+  transform-map
+  ((insta/parser exp) "abc.def AND abc.def AND (def.gh AND def.gg OR hh.ii)")) data)
 
 (defn create-filter [filter-expression]
   (instaparse.core/transform
