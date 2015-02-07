4244ceccebd9a95fa4c5f38c296806843b058f7d
Rui Yang <ryang.sei@gmail.com>
2015-02-07 13:14:49 +1100

    add test for simple value access expression and simple and expression

--- test/matchit/core_test.clj
+++ test/matchit/core_test.clj
@@ -2,6 +2,37 @@
   (:require [clojure.test :refer :all]
             [matchit.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn test-expression-with-data [expression data]
+  "Given an expression, create the according filter function and apply it to data."
+  ((create-filter expression) data))
+
+(def sample-data {
+           :name "simple name"
+           :age 25
+           :gender "male"
+           :single true
+           :vegeterian false
+           :address {
+                     :city "Melbourne"
+                     :street "david street"
+                     :post-code 1234
+                     }})
+
+(deftest single-value-expression-access-test
+  (testing "should evaluate single value access expression"
+    (is
+     (= (test-expression-with-data "abc" {:abc {:def 123}})
+        {:def 123})))
+  (testing "should evaluate single value access expression"
+    (is
+     (= (test-expression-with-data "address.city" sample-data)
+        "Melbourne")))
+  )
+
+(deftest simple-and-expression
+  (testing "should evaluate simple and expression"
+    (is
+     (= (test-expression-with-data "vegeterian AND single" sample-data)
+        false))))
+
+;; (run-all-tests #"matchit.core-test")
