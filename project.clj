6aac0bcd11e30c971ca982218c5c1c29a392e08d
Rui Yang <ryang.sei@gmail.com>
2015-07-17 22:29:27 +1000

    upgrade instaparse and clojure version

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.3.5"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [instaparse "1.4.1"]]
+  )
